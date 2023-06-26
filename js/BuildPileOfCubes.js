92e3435b700bcb7fe9b09e75498d42936caa8db0
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-26 19:31:35 +0300

    done 6 kyu kata. Build a pile of Cubes.

---
+++ js/BuildPileOfCubes.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/build-a-pile-of-cubes/
+
+function findNb(m) {
+
+    let number = 0;
+    let sum = 0;
+    while (sum < m) {
+        sum += (number + 1) ** 3;
+        number++;
+    }
+
+    return sum === m ? number : -1;
+}
