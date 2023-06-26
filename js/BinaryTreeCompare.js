41bd5b72dcffe9ca7e224a77c59f33a66fa10f7c
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-26 19:30:34 +0300

    done 6 kyu kata. Binary Tree Compare.

---
+++ js/BinaryTreeCompare.js
@@ -0,0 +1,35 @@
+// https://www.codewars.com/kata/55847fcd3e7dadc9f800005f
+
+const checkInLeft = (a, b) => {
+    if (a === null || b === null) {
+        return !((a && !b) || (!a && b));
+    }
+    if (a.val !== b.val) {
+        return false;
+    }
+    return checkInLeft(a.left, b.left);
+};
+
+const checkInRight = (a, b) => {
+    if (a === null || b === null) {
+        return !((a && !b) || (!a && b));
+    }
+    if (a.val !== b.val) {
+        return false;
+    }
+    return checkInRight(a.right, b.right);
+};
+
+function compare(a, b) {
+    if (a === null && b === null) {
+        return true;
+    }
+    if (a === null || b === null) {
+        return false;
+    }
+    if (a.val !== b.val) {
+        return false;
+    }
+
+    return checkInLeft(a.left, b.left) && checkInRight(a.right, b.right);
+}
