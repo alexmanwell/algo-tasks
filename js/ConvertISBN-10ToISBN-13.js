3bcc8117606fe88c774385a5dc20788628066780
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-06-30 22:26:46 +0300

    done 6 kyu kata. Convert ISBN-10 to ISBN-13.

---
+++ js/ConvertISBN-10ToISBN-13.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/61ce25e92ca4fb000f689fb0
+
+const PREFIX_NUMBER = 978;
+const ONE_MULTIPLIER = 1;
+const THREE_MULTIPLIER = 3;
+
+function isbnConverter(isbn) {
+    const numbers = PREFIX_NUMBER + isbn.substring(0, isbn.length - 1).split("-").join("");
+    const sum = numbers.split("").reduce((total, current, index) => {
+        return (index % 2 === 0) ? total + current * ONE_MULTIPLIER : total + current * THREE_MULTIPLIER;
+    }, 0);
+    const checkDigit = (sum % 10 !== 0) ? 10 - sum % 10 : 0;
+
+    return PREFIX_NUMBER + "-" + isbn.substring(0, isbn.length - 1) + checkDigit;
+}
