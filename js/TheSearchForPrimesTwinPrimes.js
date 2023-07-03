4dc651286d4b354b9aa487ec9c74650e9b90e9eb
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-07-03 22:13:10 +0300

    done 6 kyu kata. The Search For Primes! Twin Primes.

---
+++ js/TheSearchForPrimesTwinPrimes.js
@@ -0,0 +1,32 @@
+// https://www.codewars.com/kata/596549c7743cf369b900021b
+
+const isPrime = (n) => {
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const primes = (n) => {
+    let primes = [];
+    for (let i = 2; i <= n; i++) {
+        if (isPrime(i)) {
+            primes.push(i);
+        }
+    }
+
+    return primes;
+};
+
+const count = (primes) => {
+    return primes.reduce((acc, el, index, arr) => {
+        return (arr[index + 1] - arr[index] === 2) ? acc + 1 : acc;
+    }, 0);
+};
+
+function twinPrime(n) {
+    return count(primes(n + 1));
+}
