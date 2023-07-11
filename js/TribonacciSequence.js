d9e7a2276b2e3ff011e66280e5ad8a44689f9631
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-07-11 23:19:57 +0300

    6 kyu kata. Tribonacci Sequence.

---
+++ js/TribonacciSequence.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/556deca17c58da83c00002db
+
+const tribonacci = (signature,n) => {
+    let result = [];
+    result.push.apply(result, signature);
+    let diff = n - signature.length;
+
+    while (diff > 0) {
+        let sumNumber = 0;
+        for (let i = result.length - 1; i >= result.length - 3; i--) {
+            sumNumber += result[i];
+        }
+        result.push(sumNumber);
+        diff -= 1;
+    }
+
+    return result.slice(0, n);
+};
