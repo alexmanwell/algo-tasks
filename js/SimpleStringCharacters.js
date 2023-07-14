07be6a5e3ce2b0c2c14a13f3a36e20d0ce5f9fbe
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-07-14 22:16:29 +0300

    7 kyu kata. Simple String Characters.

---
+++ js/SimpleStringCharacters.js
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5a29a0898f27f2d9c9000058
+
+function solve(s){
+    let characters = [0, 0, 0, 0];
+    for (let i = 0; i < s.length; i++) {
+        const c = s.substring(i, i + 1);
+        if ((/[A-Z]/).test(c)) {
+            characters[0] += 1;
+        } else if ((/[a-z]/).test(c)) {
+            characters[1] += 1;
+        } else if ((/^\d$/).test(c)) {
+            characters[2] += 1;
+        } else {
+            characters[3] += 1;
+        }
+    }
+
+    return characters;
+}
