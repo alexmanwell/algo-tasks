d61f77ec972f04e1c100a0d27f524d3db76b5a9b
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-07-21 21:48:37 +0300

    done 6 kyu kata. Your Order, Please.

---
+++ codewars/js/YourOrderPlease.js
@@ -0,0 +1,18 @@
+// https://www.codewars.com/kata/55c45be3b2079eccff00010f
+
+const order = (words) => {
+  const values = words.split(' ');
+  let results = [];
+
+  for (let i = 0; i < values.length; i++) {
+    const characters = values[i].split('');
+    for (let j = 0; j < characters.length; j++) {
+      if (characters[j].match("[1-9]")) {
+        const index = characters[j];
+        results[index] = values[i];
+      }
+    }
+  }
+
+  return results.slice(1, results.length).join(' ');
+};
