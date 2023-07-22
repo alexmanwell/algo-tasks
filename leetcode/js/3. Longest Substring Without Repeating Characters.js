8527418ee62880d86c223e804ca0cd547db88810
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-07-22 05:09:30 +0300

    added second solution.

--- leetcode/js/3. Longest Substring Without Repeating Characters.js
+++ leetcode/js/3. Longest Substring Without Repeating Characters.js
@@ -58,3 +58,28 @@ const findIndex = (symbol, arr) => {
 
   return index;
 };
+
+/*
+Second solution
+*/
+const lengthOfLongestSubstring = (s) => {
+  const seen = new Map();
+  let start = 0;
+  let maxLen = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (seen.has(s[i])) {
+      start = Math.max(seen.get(s[i]) + 1, start);
+    }
+    seen.set(s[i], i);
+    maxLen = Math.max(i - start + 1, maxLen);
+  }
+
+  return maxLen;
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring(" "));
+console.log(lengthOfLongestSubstring("dvdf"));
+console.log(lengthOfLongestSubstring("au"));
