05ab70e0889c0adf942bb32b7eff9062876590ab
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-02 13:40:09 +0300

    done edabit task. Function Factory.

---
+++ edabit/js/Function Factory.js
@@ -0,0 +1,9 @@
+// https://edabit.com/challenge/ENWFBL4jbTgLbSqwS
+
+const makePlusFunction = (baseNum) => {
+  return (n) => {
+    return n + baseNum;
+  }
+};
+
+// const makePlusFunction = baseNum => num => baseNum + num;
