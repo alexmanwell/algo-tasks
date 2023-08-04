8fa6ab2b67e47fd488600f2270f420eecaa7a180
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-04 16:19:55 +0300

    done leetcode task. Find The Maximum Achievable Number.

---
+++ leetcode/js/2769. Find the Maximum Achievable Number.js
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-the-maximum-achievable-number/
+
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+const theMaximumAchievableX = (num, t) => {
+  return num + 2 * t
+};
