316294b704e2be6a42f987fdf4c731a4406d28ed
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-13 21:46:12 +0300

    done 6 kyu kata . Separating Strings.

---
+++ codewars/js/SeparatingStrings.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5977ef1f945d45158d00011f
+
+const maxLengthWord = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    max = (arr[i].length > max) ? arr[i].length : max;
+  }
+  return max;
+};
+
+function sepStr(str) {
+  let words = str.split(" ");
+  let length = maxLengthWord(words);
+  let result = [];
+  for (let col = 0; col < length; col++) {
+    let line = [];
+    for (let word of words) {
+      const letters = word.split("");
+      if (letters[col] === undefined) {
+        line.push('');
+      } else {
+        line.push(letters[col]);
+      }
+    }
+    result.push(line);
+  }
+
+  return result;
+}
