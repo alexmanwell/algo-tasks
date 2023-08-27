38b34e08e7aa6d4a138381688c1af2f15fd0f1b6
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-27 23:52:45 +0300

    done 6 kyu kata. The Supermarket Queue.

---
+++ codewars/js/TheSupermarketQueue.js
@@ -0,0 +1,21 @@
+const queueTime = (customers, n) => {
+  if (n <= 0) {
+    throw new Error("Number of tills should be a positive integer.");
+  }
+
+  if (customers.length === 0) {
+    return 0;
+  }
+
+  const tills = new Array(n).fill(0);
+  let totalTime = 0;
+
+  for (const customerTime of customers) {
+    const minTimeTill = Math.min(...tills);
+    totalTime = Math.max(totalTime, minTimeTill) + customerTime;
+    const index = tills.indexOf(minTimeTill);
+    tills[index] += customerTime;
+  }
+
+  return Math.max(...tills);
+};
