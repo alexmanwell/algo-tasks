94e802915e04d3258002e5a9418511de4e44198a
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-29 00:01:42 +0300

    Done another solution for Create Phone Number.

--- codewars/js/CreatePhoneNumber.js
+++ codewars/js/CreatePhoneNumber.js
@@ -1,5 +1,18 @@
 // https://www.codewars.com/kata/525f50e3b73515a6db000b83/train/javascript
 
+/*
 const createPhoneNumber = (numbers) => {
   return `(${numbers.slice(0, 3).join("")}) ${numbers.slice(3, 6).join("")}-${numbers.slice(6).join("")}`;
+};
+*/
+
+// Another solution
+
+const createPhoneNumber = (numbers) => {
+  let format = "(xxx) xxx-xxxx";
+  for (let i = 0; i < numbers.length; i++) {
+    format = format.replace('x', numbers[i]);
+  }
+
+  return format;
 };
