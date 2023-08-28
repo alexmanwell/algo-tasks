9edc669f105ad874eea9b1be2aa7cb484916731a
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-08-29 01:17:22 +0300

    done another solution Who Likes It task.

--- codewars/js/WhoLikesIt.js
+++ codewars/js/WhoLikesIt.js
@@ -23,3 +23,15 @@ const likes = (names) => {
 
   return (names.length < 2) ? text.concat(" likes this") : text.concat(" like this");
 };
+
+// Another solutions.
+
+const likes = (names) => {
+  return {
+    0: 'no one likes this',
+    1: `${names[0]} likes this`,
+    2: `${names[0]} and ${names[1]} like this`,
+    3: `${names[0]}, ${names[1]} and ${names[2]} like this`,
+    4: `${names[0]}, ${names[1]} and ${names.length - 2} others like this`,
+  }[Math.min(4, names.length)]
+};
