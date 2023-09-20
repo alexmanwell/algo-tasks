ebe10619349168d43d618af2927aec13028883c9
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-09-21 01:13:22 +0300

    refactoring task. Make more readable code.

--- codewars/js/WeIrD StRiNgCaSe.js
+++ codewars/js/WeIrD StRiNgCaSe.js
@@ -1,17 +1,9 @@
 // https://www.codewars.com/kata/52b757663a95b11b3d00062d
 
-const toWeirdCase = (str) => {
-  return str
-    .split(" ")
-    .map((letters) => {
-      return letters
-        .split("")
-        .map((letter, index) =>
-          (letter === " ") || (index % 2 !== 0)
-            ? letter
-            : letter.toUpperCase()
-        )
-        .join("")
-    })
-    .join(" ");
+const toWeirdCase = (sentence) => {
+  return sentence.split(" ").map((word) => {
+    return word.split("").map((letter, index) => {
+      return index % 2 !== 0 ? letter : letter.toUpperCase()
+    }).join("")
+  }).join(" ");
 };
