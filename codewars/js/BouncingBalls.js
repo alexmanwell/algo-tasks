48358982ec0e05dd92f9520088e754ea51209dea
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-09-25 20:28:16 +0300

    done 6 kyu kata. Bouncing Balls.

---
+++ codewars/js/BouncingBalls.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/5544c7a5cb454edb3c000047
+
+const bouncingBall = (h, bounce, window) => {
+  let rebounds = -1;
+  if (bounce > 0 && bounce < 1) {
+    while (h > window) {
+      rebounds += 2;
+      h *= bounce;
+    }
+  }
+
+  return rebounds;
+};
