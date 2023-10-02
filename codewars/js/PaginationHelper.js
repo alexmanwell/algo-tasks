5862a056f5cccfa9731a1c37955061a339cd6633
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-02 18:10:30 +0300

    done 5 kyu kata. Pagination helper.

---
+++ codewars/js/PaginationHelper.js
@@ -0,0 +1,29 @@
+class PaginationHelper {
+  #collection;
+  #itemsPerPage;
+
+  constructor(collection, itemsPerPage) {
+    this.#collection = collection;
+    this.#itemsPerPage = itemsPerPage;
+  }
+
+  itemCount() {
+    return this.#collection.length;
+  }
+
+  pageCount() {
+    return Math.ceil(this.itemCount() / this.#itemsPerPage);
+  }
+
+  pageItemCount(pageIndex) {
+    return (pageIndex > this.pageCount() - 1 || pageIndex < 0)
+      ? -1
+      : this.#itemsPerPage - Math.ceil((((pageIndex + 1) * this.#itemsPerPage) % this.itemCount()) % this.#itemsPerPage);
+  }
+
+  pageIndex(itemIndex) {
+    return (itemIndex > this.itemCount() - 1 || itemIndex < 0)
+      ? -1
+      : Math.ceil((itemIndex + 1) / this.#itemsPerPage) - 1;
+  }
+}
