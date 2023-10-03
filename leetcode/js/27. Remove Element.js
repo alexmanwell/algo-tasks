6b795e4cda7fe9d1f3ad440e2b4ad109fe960eb2
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-03 21:01:44 +0300

    done leetcode task. Remove Element.

---
+++ leetcode/js/27. Remove Element.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/remove-element/
+
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = (nums, val) => {
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[count] = nums[i];
+      count++;
+    }
+  }
+
+  return count;
+};
