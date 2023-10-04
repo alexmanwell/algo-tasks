e91c0c8349d995c94d48d144d3843019f1914dd3
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-04 23:23:15 +0300

    fix bug in previous solution.

--- leetcode/js/26. Remove Duplicates from Sorted Array.js
+++ leetcode/js/26. Remove Duplicates from Sorted Array.js
@@ -2,20 +2,16 @@
 
 /**
  * @param {number[]} nums
- * @return {number[]}
+ * @return {number}
  */
 const removeDuplicates = (nums) => {
-  const result = [nums[0]];
-  for (let i = 0; i < nums.length - 1; i++) {
-    const current = nums[i];
-    const next = nums[i + 1];
-    if (current !== next) {
-      result.push(next);
+  for (let i = nums.length - 1; i > 0; i--) {
+    if (nums[i] === nums[i - 1]) {
+      nums.splice(i, 1);
     }
   }
-
-  return result;
+  return nums.length;
 };
 
-console.log(removeDuplicates([1,1,2]));
-console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]));
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
