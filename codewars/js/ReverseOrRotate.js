6b655b94f9e2ebc3561372cd3d86f487dbae8c3e
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-06 23:41:35 +0300

    fix solution.

--- codewars/js/ReverseOrRotate.js
+++ codewars/js/ReverseOrRotate.js
@@ -1,36 +1,19 @@
 // https://www.codewars.com/kata/56b5afb4ed1f6d5fb0000991
 
-const separate = (str, sz) => {
-  const result = [];
-  let substring = "";
-  str.split("").forEach((c, index) => {
-    substring += c;
-    if ((index + 1) % sz === 0) {
-      result.push(Number(substring));
-      substring = "";
-    }
-  });
+const testCubes = s => Array.prototype.reduce.call(s, (acc, val) => acc + Number(val) ** 3, 0) % 2 === 0;
+const strReverse = s => [...s].reverse().join("");
+const rotateLeft = s => s.slice(1) + s.slice(0, 1);
 
-  return result;
-};
 
-const revrot = (str, sz) => {
-  let result = [];
-  if (sz === 0 || (str.length === 0) || (str.length < sz)) {
-    result = null;
+const revrot = (str, size) => {
+  let len = str.length;
+  if (size < 1 || !str || size > len) return "";
+  if (len === 1 && size === 1) return str;
+  if (/\D/.test(str)) {
+    throw Error("Invalid numeric string");
   }
-
-  if (result) {
-    const numbers = separate(str, sz);
-    numbers.forEach((number, index) => {
-      const digits = number.toString().split("");
-      let sum = digits.map(Number).reduce((acc, digit) => acc + Math.pow(digit, 3), 0);
-      result[index] =
-        (sum % 2 === 0)
-          ? Number(digits.reverse().join(""))
-          : Number([...digits.slice(1), digits[0]].join(""));
-    });
-  }
-
-  return result ? result.join("") : "";
+  let pat = new RegExp(`.{${size}}`, 'g');
+  return str.match(pat).map(x =>
+    testCubes(x) ? strReverse(x) : rotateLeft(x)
+  ).join("");
 };
