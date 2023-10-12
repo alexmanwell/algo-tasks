4b06647a3330b58d83f83789dda41d490f7cf165
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-12 05:17:08 +0300

    done 6 kyu kata. Simple Fun #115: Factor Sum.

--- codewars/js/SimpleFun115FactorSum.js
+++ codewars/js/SimpleFun115FactorSum.js
@@ -1,13 +1,37 @@
 // https://www.codewars.com/kata/589d2bf7dfdef0817e0001aa
 
-const factorSum = (n) => {
-  let sum = 0;
-  let divider = 2;
-  while (n !== 1) {
-    if (n % divider === 0) {
-      sum += divider;
-    } else {
-      divider++;
+const isPrime = (n) => {
+  let result = true;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      result = false;
+      break;
     }
   }
+
+  return result;
 };
+
+const factorSum = (number) => {
+  if (number > 5) {
+    while (!isPrime(number)) {
+      let sum = 0;
+      let index = 2;
+      while (index <= number) {
+        if (number % index === 0) {
+          sum += index;
+          number /= index;
+        } else {
+          index++;
+        }
+      }
+      number = sum;
+    }
+  }
+
+  return number;
+};
+
+console.log(factorSum(156));
+console.log(factorSum(31));
+console.log(factorSum(4));
