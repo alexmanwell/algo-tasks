1e0672eee9b638a6bbef1543b7b35045d46a0573
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-27 04:23:10 +0300

    remove extra test data.

--- codewars/js/GroupedByCommas.js
+++ codewars/js/GroupedByCommas.js
@@ -25,12 +25,3 @@ const groupByCommas = (n) => {
 
   return result.join("");
 };
-
-console.log(groupByCommas(1), '1');
-console.log(groupByCommas(10), '10');
-console.log(groupByCommas(100), '100');
-console.log(groupByCommas(1000), '1,000');
-console.log(groupByCommas(10000), '10,000');
-console.log(groupByCommas(100000), '100,000');
-console.log(groupByCommas(1000000), '1,000,000');
-console.log(groupByCommas(35235235), '35,235,235');
