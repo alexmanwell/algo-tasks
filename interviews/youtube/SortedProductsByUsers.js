57d2a3cfa60b34f869ca0a0bb3ea6475af9be6e4
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-10-30 17:49:36 +0300

    done second solution. Sorted products by users.

--- interviews/youtube/SortedProductsByUsers.js
+++ interviews/youtube/SortedProductsByUsers.js
@@ -1,15 +1,39 @@
 // https://www.youtube.com/watch?v=-nOG9Mm0za0
 
-const remapData = (data) => {
+/*const remapData = (data) => {
   const result = new Map();
   for (let items of data) {
     const [product, users] = [items[0], items.slice(1)];
     for (let user of users) {
-      result.set(user, result.has(user) ? [...result.get(user), product] : [product]);
+      result.set(user,
+        result.has(user)
+          ? [...result.get(user), product]
+          : [product]
+      );
     }
   }
 
   return Object.fromEntries(result);
+};*/
+
+
+//second solution, but I think previous solution is better then this.
+const remapData = (data) => {
+  const result = data.reduce((acc, items) => {
+    const product = items[0];
+    for (let i = 1; i < items.length; i++) {
+      const user = items[i];
+      if (acc[user]) {
+        acc[user].push(product);
+      } else {
+        acc[user] = [product];
+      }
+    }
+
+    return acc;
+  }, {});
+
+  return result;
 };
 
 console.log(remapData([
