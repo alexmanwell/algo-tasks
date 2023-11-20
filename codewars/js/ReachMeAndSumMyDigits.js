245b72c1257a8824d1561a8279a1bcff34b87174
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-11-20 14:15:18 +0300

    improve solution. Better then previous solution.

--- codewars/js/ReachMeAndSumMyDigits.js
+++ codewars/js/ReachMeAndSumMyDigits.js
@@ -1,27 +1,10 @@
 // https://www.codewars.com/kata/55ffb44050558fdb200000a4
 
-const sumDigits = (number) => {
-  let sum = 0;
-  while (number !== 0) {
-    sum += number % 10;
-    number = parseInt(number / 10);
-  }
-
-  return sum;
-};
-
 const sumDigNthTerm = (start, pattern, amount) => {
-  let sequence = new Array(amount).fill(0);
   let sum = start;
-  sequence[0] = sum;
-  let count = 1;
-  while (count < amount) {
-    for (let i = 0; i < pattern.length && count < amount; i++) {
-      sum += pattern[i];
-      sequence[count] = sum;
-      count++;
-    }
+  for (let i = 0; i < amount - 1; i++) {
+    sum += pattern[i % pattern.length];
   }
 
-  return sumDigits(sequence[sequence.length - 1]);
+  return sum.toString().split("").reduce((a, b) => +a + +b, 0);
 };
