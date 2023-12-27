15ef04c20b54c20fa8210c114afb1984dea8a26a
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2023-12-28 01:30:14 +0300

    refactoring code. Change amount arguments.

--- codewars/js/FunctionComposition.js
+++ codewars/js/FunctionComposition.js
@@ -1,7 +1,7 @@
 // https://www.codewars.com/kata/5655c60db4c2ce0c2e000026
 
-const compose = (...functions) => (arg) => {
-  return functions.reduceRight((acc, cur) => {
-    return Array.isArray(acc) ? cur(...acc) : cur(acc)
-  }, arg)
+const compose = (...fn) => (...args) => {
+  return fn.reduceRight((acc, item) => {
+    return Array.isArray(acc) ? item(...acc) : item(acc)
+  }, args)
 };
