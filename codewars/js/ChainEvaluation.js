bc6e9f76b5e2b0d5e735184e9407c9759528c23b
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-01-12 23:06:33 +0300

    done 6 kyu kata. Chain Evaluation.

---
+++ codewars/js/ChainEvaluation.js
@@ -0,0 +1,21 @@
+// https://www.codewars.com/kata/545a5dd961aa4c0987000980
+
+Number.prototype.add = function (a) {
+  return this + a;
+};
+
+Number.prototype.subtract = function (a) {
+  return this - a;
+};
+
+Number.prototype.multiply = function (a) {
+  return this * a;
+};
+
+Number.prototype.divide = function (a) {
+  return this / a;
+};
+
+Number.prototype.square = function () {
+  return this ** 2;
+};
