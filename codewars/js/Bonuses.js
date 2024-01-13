66cc0a1e89abfca5d728d68b8ea06751af5f4f71
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-01-14 00:37:49 +0300

    find more simple solution. 6 kyu kata. Bonuses.

--- codewars/js/Bonuses.js
+++ codewars/js/Bonuses.js
@@ -14,3 +14,17 @@ const bonus = (arr, s) => {
 
   return result.reduce((acc, v) => acc + v, 0) === s ? result : NaN;
 };
+
+// Done more simple solution.
+const bonus = (arr, s) => {
+  let sum = 0;
+  for (const value of arr) {
+    sum += 1 / value;
+  }
+  let result = new Array(arr.length);
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = Math.round(s / sum / arr[i]);
+  }
+
+  return result;
+};
