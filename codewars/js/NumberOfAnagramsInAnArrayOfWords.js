033c81e2004e60313095a8df867a528692f42a31
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-01-26 23:55:33 +0300

    done 6 kyu kata. Number of anagrams in an array of words.

---
+++ codewars/js/NumberOfAnagramsInAnArrayOfWords.js
@@ -0,0 +1,22 @@
+// https://www.codewars.com/kata/587e18b97a25e865530000d8
+
+const ascendingSort = (str) => {
+  return str.split(" ").sort().join(" ");
+};
+
+const compareStrings = (str1, str2) => {
+  return ascendingSort(str1) === ascendingSort(str2);
+};
+
+const anagramCounter = (words) => {
+  let count = 0;
+  for (let i = 0; i < words.length - 1; i++) {
+    for (let j = i + 1; j < words.length; j++) {
+      if (compareStrings(words[i], words[j])) {
+        count++
+      }
+    }
+  }
+
+  return count;
+};
