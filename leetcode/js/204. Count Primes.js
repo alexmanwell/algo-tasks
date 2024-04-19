1fee9ade1fc4da41597f55bce66e3e7c10615cd0
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-04-19 14:20:27 +0300

    refactoring code. 204. Count Primes. Make better algorithm find counting primes number.

--- leetcode/js/204. Count Primes.js
+++ leetcode/js/204. Count Primes.js
@@ -1,27 +1,24 @@
 // https://leetcode.com/problems/count-primes
-// TODO: This solution is perform requirements of time limit. But it doesn't good solution. I think I can find a better solution.
-
-const isPrime = (n) => {
-  for (let i = 3; i <= Math.sqrt(n); i += 2) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-
-  return n >= 2;
-}
 
 /**
  * @param {number} n
  * @return {number}
  */
 const countPrimes = (n) => {
-  let count = 1;
-  for (let i = 3; i < n; i += 2) {
-    if (isPrime(i)) {
+  let isPrimes = new Array(n).fill(true);
+  for (let i = 2; i * i < n; ++i) {
+    if (isPrimes[i]) {
+      for (let j = i; j * i < n; ++j) {
+        isPrimes[i * j] = false;
+      }
+    }
+  }
+  let count = 0;
+  for (let i = 2; i < isPrimes.length; i++) {
+    if (isPrimes[i]) {
       count++;
     }
   }
 
-  return n <= 2 ? 0 : count;
+  return count;
 };
