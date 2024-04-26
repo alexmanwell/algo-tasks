27124e064086a20e79d0624782ab43933c5f3dc2
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-04-26 15:55:27 +0300

    refactoring code. 2089. Find Target Indices After Sorting Array.

--- leetcode/js/2089. Find Target Indices After Sorting Array.js
+++ leetcode/js/2089. Find Target Indices After Sorting Array.js
@@ -6,35 +6,20 @@
  * @return {number[]}
  */
 const targetIndices = (nums, target) => {
-  const sorted = nums.sort((a, b) => a - b);
-  let left = 0;
-  let right = nums.length;
-  let result = [];
-  while (left < right) {
-    let middle = Math.floor((right + left) / 2);
-    if (sorted[middle] === target) {
-      result.unshift(middle);
-      let candidate = middle;
-      if (sorted[candidate + 1] === target) {
-        while (candidate + 1 < sorted.length && sorted[candidate + 1] === target) {
-          candidate++;
-          result.push(candidate);
-        }
-      }
-      candidate = middle;
-      if (sorted[candidate - 1] === target) {
-        while (candidate - 1 >= 0 && sorted[candidate - 1] === target) {
-          candidate--;
-          result.unshift(candidate);
-        }
-      }
-      break;
-    }
-    if (sorted[middle] < target) {
-      left = middle + 1;
+  let smaller = 0;
+  let equal = 0;
+  for (let item of nums) {
+    if (item < target) {
+      smaller++;
     } else {
-      right = middle;
+      if (item === target) {
+        equal++;
+      }
     }
+  }
+  let result = [];
+  for (let i = smaller; i < smaller + equal; i++) {
+    result.push(i);
   }
 
   return result;
