704572e7c54d28b9b23c9bdc6e82e591a244d72b
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-04-29 16:20:55 +0300

    refactoring code. 2384. Largest Palindromic Number.

--- leetcode/js/2384. Largest Palindromic Number.js
+++ leetcode/js/2384. Largest Palindromic Number.js
@@ -1,54 +1,36 @@
 // https://leetcode.com/problems/largest-palindromic-number
-// TODO: doesn't consider specific cases, for example input string: "00001105", must return output: "1005001".
+
+const countDigits = (number) => {
+  let map = new Map();
+  for (let digit of number) {
+    map.set(digit, map.has(digit) ? map.get(digit) + 1 : 1);
+  }
+
+  return map;
+}
 
 /**
  * @param {string} num
  * @return {string}
  */
 const largestPalindromic = (num) => {
-  let map = new Map();
-  for (let digit of num) {
-    map.set(digit, map.has(digit) ? map.get(digit) + 1 : 1);
-  }
-  const sorted = [...map.entries()].sort(([k1, v1], [k2, v2]) => +k2 - (+k1) || v2 - v1);
-  const oddDigit = sorted.filter(([digit, value]) => value % 2 !== 0);
-  const evenDigit = sorted.filter(([digit, value]) => value % 2 === 0);
-  let maxAmountOddDigit = oddDigit[0][1];
-  let maxDigit = 0;
-  for (let [digit, amount] of oddDigit) {
-    if (maxAmountOddDigit === amount) {
-      if (digit > maxDigit) {
-        maxDigit = digit;
+  let map = countDigits(num);
+  let middle = "";
+  let half = "";
+  for (let i = 9; i >= 0; i--) {
+    const digit = i.toString();
+    if (map.get(digit) > 1) {
+      if (digit !== "0" || (digit === "0" && half.length > 0)) {
+        half += digit.toString().repeat(Math.floor(map.get(digit)) / 2);
       }
+      map.set(digit, map.get(digit) % 2);
+    }
+    if (map.get(digit) === 1 && middle === "") {
+      middle = digit;
+      map.set(digit, map.get(digit) - 1);
     }
   }
-  let result = "";
-  if (maxAmountOddDigit > 1) {
-    for (const [digit, amount] of evenDigit) {
-      if (maxDigit > digit) {
-        const half = Math.floor(maxAmountOddDigit / 2);
-        result += maxDigit.toString().repeat(half);
-      } else {
-        const half = Math.floor(amount / 2);
-        result += digit.toString().repeat(half);
-      }
-    }
-    result += maxDigit + result.split("").reverse().join("");
-  } else if (maxAmountOddDigit === 1) {
-    if (evenDigit[0][0] !== "0") {
-      for (const [digit, amount] of evenDigit) {
-        const half = Math.floor(amount / 2);
-        result += digit.toString().repeat(half);
-      }
-    }
-    result += maxDigit + result.split("").reverse().join("");
-  } else {
-    for (const [digit, amount] of evenDigit) {
-      const half = Math.floor(amount / 2);
-      result += digit.toString().repeat(half);
-    }
-    result += result.split("").reverse().join("");
-  }
+  const result = half + middle + [...half].reverse().join("");
 
-  return result;
+  return result.length === 0 ? "0" : result;
 };
