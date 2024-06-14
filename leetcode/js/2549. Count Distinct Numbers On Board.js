214800191aec5716288006085f201dc00cb813fd
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-06-14 23:25:01 +0300

    done leetcode task. 2549. Count Distinct Numbers on Board.

---
+++ leetcode/js/2549. Count Distinct Numbers On Board.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/count-distinct-numbers-on-board
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+const distinctIntegers = (n) => {
+  return n === 1 ? 1 : n - 1;
+};
