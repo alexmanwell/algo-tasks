807c2cf273b8edf7288e3a355b1dcb594f74f237
Alexandr Manvelyan <69740260+alexmanwell@users.noreply.github.com>
2024-09-07 09:43:20 +0300

    another solution, much shorter. 2119. A Number After a Double Reversal.

--- leetcode/js/2119. A Number After a Double Reversal.js
+++ leetcode/js/2119. A Number After a Double Reversal.js
@@ -1,17 +1,9 @@
 // https://leetcode.com/problems/a-number-after-a-double-reversal
 
-const reverseNumber = (n) => {
-  return +n
-    .toString()
-    .split("")
-    .reverse()
-    .join("");
-};
-
 /**
  * @param {number} num
  * @return {boolean}
  */
 const isSameAfterReversals = (num) => {
-  return reverseNumber(reverseNumber(num)) === num;
+  return num === 0 || num % 10 !== 0;
 };
